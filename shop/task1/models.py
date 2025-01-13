2eee26818358a4ae406e9df1adff6de50062cf7c
vokashlob <58624355+vokashlob@users.noreply.github.com>
2025-01-13 11:36:38 +0300

    first commit

---
+++ shop/task1/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Buyer(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    balance = models.DecimalField(max_digits=9, decimal_places=2)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.DecimalField(max_digits=9, decimal_places=3)
+    description = models.TextField(blank=True)
+    age_limited = models.BooleanField(default=False)
+    buyer = models.ManyToManyField(Buyer, related_name='games')
+
+    def __str__(self):
+        return self.title
