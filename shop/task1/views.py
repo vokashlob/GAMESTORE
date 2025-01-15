c75d825b7b09d32422b144b1be21ddd6b154eb8d
vokashlob <58624355+vokashlob@users.noreply.github.com>
2025-01-15 11:41:18 +0300

    added values in Buyer, Game

--- shop/task1/views.py
+++ shop/task1/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
-
+# from django.views.generic import TemplateView
 # Create your views here.
+
+
+def  platform(request):
+    title = 'Главная страница'
+    context = {
+        'title': title
+    }
+    return render(request, 'fourth_task/platform.html', context)
+
+
+def games(request):
+    title = 'Игры'
+    games = ['Poker Superstars', 'The Talos Principle', 'Uncrashed', 'Liftoff', 'Portal Revolution']
+    back_url = "/platform"
+    context = {
+        'title': title,
+        'games': games,
+        'len_games': len(games),
+        'back_url': back_url
+    }
+    return render(request, 'fourth_task/games.html', context)
+
+
+def cart(request):
+    title = 'Корзина'
+    back_url = "/platform"
+    context = {
+        'title': title,
+        'back_url': back_url
+    }
+    return render(request, 'fourth_task/cart.html', context)
